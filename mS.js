043ab00d6844462ecbb458aa8dfdbccc5a18d534
safaacht <safaachtaoui@gmail.com>
2025-11-04 10:44:59 +0100

    commit

---
+++ mS.js
@@ -0,0 +1,43 @@
+// const form=document.getElementById(my-form)
+// form.innerHTML
+
+
+
+
+const Color=document.querySelectorAll(".challenges")
+Color.addEventListner(c,()=>{
+    c.style.background=red;
+    
+})
+
+const color=document.querySelector(".chal1")
+color.addEventListner("click",()=>{
+    color.style.background=red;
+    
+})
+
+const text=document.querySelector(".chal4")
+const tex=document.querySelector(".chal4").value;
+text=tex.
+    
+function msg() {
+const message=document.querySelector(".chal16")
+message.addEventListener("click",()=>{
+    alert("Hello World");
+    console.log(message);
+})
+}
+msg();
+
+
+const change=document.querySelectorAll(".challenges")
+change.addEventListener("click",()=>{
+    change.style.background=blue ;
+})
+
+
+
+const addparag=document.querySelector(".chal12")
+addparag.addEventListener("click",()=>{
+    
+})
