d7085646b7f420e4bbc2c91dce0cb8c4c0e7177d
safaacht <safaachtaoui@gmail.com>
2025-11-18 18:38:28 +0100

    committing

--- mS.js
+++ mS.js
@@ -238,9 +238,81 @@ function hideScrollingText() {
 
 
     clearTimeout(window.scrollTimeout);
-    window.scrollTimeout = setTimeout(hideScrollingText, 500);
+    window.scrollTimeout = setTimeout(hideScrollingText, 300);
  });
 
 
 
+//  On click, generate and display a random number (challenge26)
+const randomNumber=document.querySelector(".challenge26");
+const prg=document.createElement("p");
+randomNumber.appendChild(prg);
+
+randomNumber.addEventListener("click",()=>{
+  let random=Math.random();
+prg.textContent=`Num=${random}`  
+})
+
     
+
+
+// On click, toggle visibility of another div  (challenge27)
+
+const chal27=document.querySelector(".challenge27");
+chal27.addEventListener("click",()=>{
+  const isHidden=randomNumber.classList.toggle('is-hidden');
+
+  if(isHidden){
+    randomNumber.style.display="none";
+  }else{
+    randomNumber.style.display="block";
+  }
+})
+
+
+
+
+// Add input type=“color”; on change, apply it as background (challenge29)
+const chal29=document.querySelector(".challenge29");
+const input5=document.createElement("input");
+input5.type="color"
+
+chal29.appendChild(input5);
+
+chal29.addEventListener("change",()=>{
+  chal29.style.background=input5.value;
+})
+
+
+
+
+// On click, store this div’s color in localStorage (challenge30)
+const chal30=document.querySelector(".challenge30");
+
+const color=chal30.style.background="purple";
+
+chal30.addEventListener("click",()=>{
+  const save= localStorage.setItem("color","purple");
+})
+
+
+// On page load, restore color from localStorage (challenge31)
+const chal31=document.querySelector(".challenge31");
+
+window.addEventListener("load",()=>{
+  const restore=(localStorage.getItem("color"));
+
+  if(restore){
+    chal31.style.background=restore;
+  }
+})
+
+
+// Create an object {name, age} and display its properties (challenge32)
+const chal32=document.querySelector(".challenge32");
+
+const obj={nom:"Safaa",age:21, city:"Casablanca"}
+console.log({ nom: obj.nom, age: obj.age });
+
+
+chal32.innerHTML=`Nom:${obj.nom}, Age:${obj.age}`
